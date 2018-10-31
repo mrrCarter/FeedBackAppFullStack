bf6307b2abcb462092884406060d8e7f19f65dc6
Carther S Theogene <carther@mac-w20-575-07.mit.edu>
2018-10-31 16:55:01 -0400

    deleted the indexlock file

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("This is a confirmation that i have received your request");
+});
+
+const PORT = process.env.PORT || 5001;
+
+//localhost:5001
+app.listen(PORT);
