11652332bf58e53d8120db79bd3fc6229e3cedf9
Carther S Theogene <carther@mac-w20-575-07.mit.edu>
2018-11-02 16:07:21 -0400

    second commit without node modules

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("This is a confirmation that i have received your request");
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 //localhost:5001
 app.listen(PORT);
