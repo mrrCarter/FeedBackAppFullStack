bb8f2a3125be4407d6f5ac760175ea1856d136db
Carther S Theogene <carther@mac-w20-575-10.mit.edu>
2018-11-09 01:07:04 -0500

    Milestone: done with passport configuration. Understood the flow of signing in using google oauth. printed out some info from google json profile information.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
+const keys = require("/Users/carther/Desktop/carther/FeedBackApp/server/FeedBackAppFullStack/server/config/key");
+
 const app = express();
 
 //test route handler... should be deleted
@@ -15,7 +17,37 @@ const app = express();
 //new google strategy creates a new istance of the google passport strategy
 //ClientID:
 //clientSecret: in config/keys
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token", accessToken);
+      console.log("refresh token", refreshToken);
+      console.log("profile", profile);
+      console.log("Image", profile.url);
+      console.log("HAHA HE'S NAME IS:", profile.displayName);
+    }
+  )
+);
+
+//route handler for the authentification and specify the scope to be profile and email
+
+app.get(
+  "/auth/google", //whenever a user comes to this route
+  passport.authenticate("google", {
+    //google strategy has an internal identifier called 'google'
+    //hey passport, attempt to authenticate the user who is coming in on this route (/auth/google) and use the strategy called 'google'
+    scope: ["profile", "email"]
+  })
+);
+
+//now we need another route handler for when the user chooses the account to sign in with. at this point passport will echange the 'code=4' or whatever the code is in the url to the code google has
+app.get("/auth/google/callback", passport.authenticate("google"));
+//after we get the permission, google will give us an access token that we can then use to get information from the user.
 
 const PORT = process.env.PORT || 5000;
 
